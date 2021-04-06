0993c036504eeb76bc6e8c2294c877e2400ddcdd
Flymardones <christophermardonesandersen@gmail.com>
2021-04-06 17:05:45 +0200

    test

--- src/project1/class1.java
+++ src/project1/class1.java
@@ -4,5 +4,6 @@ public class class1 {
     public static void main(String[] args) {
         System.out.println("hello world");
         System.out.println("hello world");
+        System.out.println("Goodbye");
     }
 }
