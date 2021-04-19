4d807ac4e7cbc2fac7541bb471d47b53d5c31803
Rasmus Lundahl Nielsen <rlnielsen1@hotmail.com>
2021-04-19 17:09:40 +0200

    finalized hospitalized class

--- src/project1/testDemo.java
+++ src/project1/testDemo.java
@@ -1,5 +1,6 @@
 package project1;
 
+import Filehandler.HospitalizedFileHandler;
 import Filehandler.KeyNumbersFileHandler;
 import java.util.ArrayList;
 
@@ -9,6 +10,12 @@ public class testDemo {
         ArrayList<KeyNumbers> stats = KeyNumbersFileHandler.getKeyNumbersFromFile("Covid19Stats/Kommunalt_DB/01_noegletal.csv");
         Phase phase1 = new Phase("1" ,stats);
         phase1.printKeyNumbers();
+
+        HospitalizedFileHandler hospitalizedFileHandler = new HospitalizedFileHandler();
+        ArrayList<Hospitalized> test = HospitalizedFileHandler.getAgeGroupFromFile("Covid19Stats/Kommunalt_DB/03_indlæggelser_pr_aldersgrp.csv");
+        ListHospitalized listHospitalized = new ListHospitalized(test);
+        ArrayList<Hospitalized> hospitalized = listHospitalized.getAgeGroups();
+        System.out.println(hospitalized.get(2).getHospitalized());
 
         //ArrayList<KeyNumbers> keyNumbers = phase1.getKeyNumbers();
         //System.out.println(keyNumbers.get(3).getDate());
