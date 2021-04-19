a5c0a0521bec68f063ede3e2a6c8a6caef8ff4ed
gurkefar <jonasjensen04@gmail.com>
2021-04-19 16:54:54 +0200

    Hospitalized file handler fixed

--- src/Filehandler/HospitalizedFileHandler.java
+++ src/Filehandler/HospitalizedFileHandler.java
@@ -1,7 +1,6 @@
 package Filehandler;
 
 import project1.Hospitalized;
-import project1.KeyNumbers;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
@@ -33,8 +32,8 @@ public class HospitalizedFileHandler {
         return ageGroups;
     }
 
-    private static KeyNumbers parseNumbers(String inputString) {
+    private static Hospitalized parseNumbers(String inputString) {
         String[] tokens = inputString.split(";");
-        return new KeyNumbers(tokens[0],tokens[1],tokens[2],tokens[3],tokens[4],tokens[5],tokens[6],tokens[7],tokens[8],tokens[9],tokens[10],tokens[11],tokens[12],tokens[13],tokens[14],tokens[15],tokens[16],tokens[17],tokens[18],tokens[19],tokens[20],tokens[21],tokens[22],tokens[23]);
+        return new Hospitalized(tokens[0],tokens[1]);
     }
 }
